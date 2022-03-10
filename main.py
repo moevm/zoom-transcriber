5b6893e87d0fa4558795f81b4b3568ba6d3e5e18
SuperSolik <serpetr99@gmail.com>
2022-03-10 18:21:25 +0300

    remove ffmpeg python && use Popen directly

--- main.py
+++ main.py
@@ -3,13 +3,20 @@ import os
 import subprocess
 import time
 from argparse import ArgumentParser
+from functools import lru_cache
 from pathlib import Path
 from typing import NamedTuple, Union
 
-import ffmpeg
 from vosk import KaldiRecognizer, Model, SetLogLevel
 
-SetLogLevel(0)
+SetLogLevel(-1)
+
+
+QUESTION_RULES = [
+    lambda text: "вопрос" in text and "спасибо" not in text,
+    lambda text: "а что такое" in text,
+    lambda text: "в чем заключается" in text,
+]
 
 
 class VoskResult(NamedTuple):
@@ -17,30 +24,39 @@ class VoskResult(NamedTuple):
     end: float
     text: str
     speaker: str
+    is_question: bool
 
 
+@lru_cache(maxsize=5)
 def get_model(model_path: str) -> KaldiRecognizer:
+    print(f"Init {model_path} model loading")
     model = Model(model_path)
-    rec = KaldiRecognizer(model, 16000)
-    rec.SetWords(True)
-    print("model loaded")
-    return rec
+    print("Model loading completed")
+    return model
 
 
-def process_vosk_result(res: dict, speaker_name: str) -> VoskResult:
+def process_vosk_result(res: dict, speaker_name: str) -> Model:
     if not (words := res.get("result")):
         return
+
+    text = res["text"]
+    is_question = False
+    for rule in QUESTION_RULES:
+        if rule(text):
+            is_question = True
+            break
 
     return VoskResult(
         start=words[0]["start"],
         end=words[-1]["end"],
-        text=res["text"],
+        text=text,
         speaker=speaker_name,
+        is_question=is_question,
     )
 
 
 def process_audiofile(
-    file_path_raw: Union[str, Path], rec: KaldiRecognizer, speaker_name: str = None
+    file_path_raw: Union[str, Path], model: Model, speaker_name: str = None
 ) -> tuple[list[VoskResult], str]:
     result: list[VoskResult] = []
     file_path = Path(file_path_raw).absolute()
@@ -49,20 +65,37 @@ def process_audiofile(
     print(f"Started processing file {file_name}")
     start_time = time.time()
 
-    process: subprocess.Popen = (
-        ffmpeg.input(file_path)
-        .filter("highpass", f=200)
-        .filter("lowpass", f=3000)
-        .output("-", format="s16le", ac=1, ar=16000)
-        .run_async(pipe_stdout=True, pipe_stderr=True, quiet=True)
+    process = subprocess.Popen(
+        [
+            "ffmpeg",
+            "-loglevel",
+            "quiet",
+            "-i",
+            file_path,
+            "-ar",
+            "16000",
+            "-ac",
+            "1",
+            "-filter:a",
+            "highpass=f=200, lowpass=f=3000",
+            "-f",
+            "s16le",
+            "-",
+        ],
+        bufsize=0,
+        stdout=subprocess.PIPE,
     )
 
     if speaker_name is None:
         speaker_name = file_name
+
+    rec = KaldiRecognizer(model, 16000)
+    rec.SetWords(True)
+
     i = 0
-    while data := process.stdout.read(4000):
-        if rec.AcceptWaveform(data):
-            recognized_data = json.loads(rec.FinalResult())
+    while bytes_data := process.stdout.read(4000):
+        if rec.AcceptWaveform(bytes_data):
+            recognized_data = json.loads(rec.Result())
             processed_result = process_vosk_result(recognized_data, speaker_name)
             if processed_result is not None:
                 result.append(processed_result)
@@ -72,12 +105,13 @@ def process_audiofile(
 
         i += 1
 
+    process.wait()
+
     recognized_data = json.loads(rec.FinalResult())
     processed_result = process_vosk_result(recognized_data, speaker_name)
     if processed_result is not None:
         result.append(processed_result)
 
-    rec.Reset()
     elapsed_time = time.time() - start_time
     print(f"Finished processig file {file_name}, elapsed time {elapsed_time}s")
 
@@ -96,17 +130,15 @@ def process_dir(dir_path_raw: str, model_path: str) -> None:
         speaker_result, _ = process_audiofile(audio_file, get_model(model_path))
         meeting_result.extend(speaker_result)
 
-        # with open(dir_name + f"/{file_name}.json", "w", encoding="utf-8") as f:
-        #     json.dump(
-        #         [r._asdict() for r in speaker_result], f, ensure_ascii=False, indent=2
-        #     )
-
     meeting_result.sort(key=lambda r: r.start)
 
     with open(dir_name + "/result.json", "w", encoding="utf-8") as f:
         json.dump(
             [r._asdict() for r in meeting_result], f, ensure_ascii=False, indent=2
         )
+
+    model = get_model(model_path)
+    del model
 
     elapsed_time = time.time() - start_time
     print(f"Finished processing directory {dir_path}, elapsed time: {elapsed_time}s")
